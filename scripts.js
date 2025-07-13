97d071e9c73adc23ea43734e9aef20557bddffa5
wet moss <svyatoivo@gmail.com>
2025-07-13 23:02:03 +0300

    full site

--- scripts.js
+++ scripts.js
@@ -1,58 +1,26 @@
-const track = document.querySelector('.slider__track');
-const btnPrev = document.querySelector('.slider__arrow--prev');
-const btnNext = document.querySelector('.slider__arrow--next');
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.swiper').forEach((slider) => {
+        const isBrandSlider = slider.classList.contains('brand-slider');
 
-const slideWidth = document.querySelector('.slider__item').offsetWidth;
-let isMoving = false;
-
-const maxVisibleSlides = 4;
-const slides = track.children;
-
-let disable = false;
-
-function updateArrowState() {
-    disable = slides.length <= maxVisibleSlides;
-    btnPrev.classList.toggle('slider__arrow--disabled', disable);
-    btnNext.classList.toggle('slider__arrow--disabled', disable);
-}
-
-updateArrowState();
-
-function moveNext() {
-    if (isMoving || disable) return;
-    isMoving = true;
-
-    track.style.transition = 'transform 0.4s ease';
-    track.style.transform = `translateX(-${slideWidth}px)`;
-
-    track.addEventListener('transitionend', function handler() {
-        track.appendChild(track.firstElementChild);
-        track.style.transition = 'none';
-        track.style.transform = 'translateX(0)';
-        isMoving = false;
-
-        track.removeEventListener('transitionend', handler);
+        new Swiper(slider, {
+            slidesPerView: isBrandSlider ? 5 : 4,
+            spaceBetween: 24,
+            loop: true,
+            navigation: {
+                nextEl: slider.querySelector('.swiper-button-next'),
+                prevEl: slider.querySelector('.swiper-button-prev'),
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1.2,
+                },
+                640: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: isBrandSlider ? 5 : 4,
+                }
+            }
+        });
     });
-}
-
-function movePrev() {
-    if (isMoving || disable) return;
-    isMoving = true;
-
-    track.style.transition = 'none';
-    track.insertBefore(track.lastElementChild, track.firstElementChild);
-    track.style.transform = `translateX(-${slideWidth}px)`;
-
-    requestAnimationFrame(() => {
-        track.style.transition = 'transform 0.4s ease';
-        track.style.transform = 'translateX(0)';
-    });
-
-    track.addEventListener('transitionend', () => {
-        isMoving = false;
-    }, { once: true });
-}
-
-
-btnNext.addEventListener('click', moveNext);
-btnPrev.addEventListener('click', movePrev);
+});
